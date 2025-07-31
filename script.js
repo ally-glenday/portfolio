c1ef09a44971731e74871492aed53cf5b63ba25c
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2025-07-31 11:23:14 +0900

    Nav not complete but working

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function toggleNavBar(){
-    var navButton = document.getElementById("nav-button");
-    navButton.classList.toggle("display-block");
-}
+// function toggleNavBar(){
+//     var navButton = document.getElementById("nav-button");
+//     navButton.classList.toggle("display-block");
+// }
