601bb9ba27844f4eadb2886992487641f5f8fd89
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2025-08-06 08:24:21 +0900

    Hamburger menu bug fixed

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navBar = document.querySelector(".nav-bar");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    navBar.classList.toggle("active");
+
 })
+
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
     hamburger.classList.remove("active");
