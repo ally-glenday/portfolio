8acdde00a658147c621c55b894cccadcc35b8ef7
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2025-08-06 09:21:39 +0900

    Add p5.js sketch!

---
+++ sketch.js
@@ -0,0 +1,35 @@
+// Basile Pesin
+// http://vertmo.github.io
+
+// MetaBalls : p5.js implementation
+
+var blobs = []
+
+function setup() {
+  createCanvas(600, 600);
+  colorMode(HSB);
+  for (i = 0; i < 15; i++) blobs.push(new Blob(random(0, width), random(0, height)));
+}
+
+function draw() {
+  background(50);
+
+  loadPixels();
+  for (x = 0; x < width; x++) {
+    for (y = 0; y < height; y++) {
+      let sum = 0;
+      for (i = 0; i < blobs.length; i++) {
+        let xdif = x - blobs[i].x;
+        let ydif = y - blobs[i].y;
+        let d = sqrt((xdif * xdif) + (ydif * ydif));
+        sum += 30 * blobs[i].r / d;
+      }
+      set(x, y, color(sum, 50, 100));
+    }
+  }
+  updatePixels();
+
+  for (i = 0; i < blobs.length; i++) {
+    blobs[i].update();
+  }
+}
