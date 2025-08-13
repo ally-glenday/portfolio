6c3554d674fe49e2742f7910dd402886ef2d1264
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2025-08-13 11:37:32 +0900

    Added z-index +1 to nav to fix text bug on scroll

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navBar = document.querySelector(".nav-bar");
+const header = document.querySelector("header");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
     navBar.classList.toggle("active");
-
+    header.style.backgroundColor("red");
+    
 })
 
 
